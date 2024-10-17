03ac2f86d5bcf2c946857dbf60ba0d0e9268197c
campo22 <miguelcampodiaz@gmail.com>
2024-10-17 09:39:01 -0500

    integrando  la api

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ApiService } from '../../services/api.service';
 
 @Component({
   selector: 'app-products',
@@ -7,6 +8,18 @@ import { Component } from '@angular/core';
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
+  productList: any[] = [];
 
+  private _apiService = inject(ApiService);
+
+
+  ngOnInit(): void {
+    this._apiService.getProducts().subscribe({
+      next: (data) => {
+        this.productList = data;
+        console.log(data);
+      }
+    });
+  }
 }
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,9 +1,28 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  private _http = inject(HttpClient);
+  private baseUrl = 'https://fakestoreapi.com/products';
+
+
+  getProducts(): Observable<any> {
+    return this._http.get(this.baseUrl);
+  }
+
+  getById(id: number): Observable<any> {
+    return this._http.get(`${this.baseUrl}/${id}`);
+  }
+
+  posProduct(product: any): Observable<any> {
+    return this._http.post(this.baseUrl, product);
+  }
+
+
+
 }
